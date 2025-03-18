b8830dc61df7f68ba4bc831d6dcda6b3d2b41abc
thuynh808 <thuynh808@streetrack.org>
2025-03-17 23:41:44 -1000

    updated artifactory install

--- generate_keys.sh
+++ generate_keys.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 VAULT_PASS="password"
-ADMIN_PASS="password"
 
 # Generate master and join keys
 MASTER_KEY=$(openssl rand -hex 32)
@@ -14,7 +13,6 @@ chmod 600 ~/.vault_pass.txt
 echo ""
 echo "✅ Use the following for Ansible Vault file (secret-vars.yml):"
 echo "--------------------------------"
-echo "artifactory_admin_password: $ADMIN_PASS"
 echo "master_key: $MASTER_KEY"
 echo "join_key: $JOIN_KEY"
 echo "--------------------------------"
